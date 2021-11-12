a67cb9471f5e4e8a550be5aa834fd2f6ec0393fa
Dapborlang <dapbor88@gmail.com>
2021-11-12 18:52:47 +0530

    added edit in view

--- src/Http/Controllers/FormBuilderController.php
+++ src/Http/Controllers/FormBuilderController.php
@@ -99,9 +99,40 @@ class FormBuilderController extends Controller
         //
     }
 
-    public function edit($id)
+    public function edit($id,$cid)
     {
-        //
+        $formMaster=FormMaster::findOrFail($id);
+        $model= $formMaster->model::findOrFail($cid);
+        $columns = \DB::connection()->getSchemaBuilder()->getColumnListing($formMaster->table_name);
+
+        $masterKey=json_decode($formMaster->master_keys, true);
+        $master=array();
+        if(sizeof((array)$masterKey)>0)
+        {
+            foreach (array_keys($masterKey) as $item) {
+                $data=$item::all();
+                $master[$masterKey[$item][2]]=array($data,$masterKey[$item][0],$masterKey[$item][1],$masterKey[$item][3]);
+            }
+        }
+
+        $foreign=json_decode($formMaster->foreign_keys, true);
+        $select=array();
+        if(sizeof((array)$foreign)>0)
+        {
+            foreach (array_keys($foreign) as $key) {
+                $data=$key::all();
+                $select[$foreign[$key][0]]=array($data,$foreign[$key][1],$foreign[$key][2]);
+            }
+        }
+        $exclude=json_decode($formMaster->exclude, true);
+        $attribute=json_decode($formMaster->attribute, true);
+        if($formMaster->route=='formbuilder')
+        {
+            return view('formbuilder::formbuilder.edit',compact('columns','formMaster','select','master','exclude','attribute','model'));
+        }
+        else{
+
+        }
     }
 
     public function update(Request $request, $id)
