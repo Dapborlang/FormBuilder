d2513233b9713c51b62234d8c680ceed298cc921
Dapborlang <dapbor88@gmail.com>
2021-11-13 20:36:53 +0530

    remove masterkeys and add attributes

--- src/views/formbuilder/create.blade.php
+++ src/views/formbuilder/create.blade.php
@@ -43,11 +43,13 @@
                                 </select>
                                 @else
                                     @if(!isset($attribute['type'][$item]))
-                                        <input type="text" class="form-control  form-control-sm" id="{{$item}}" name="{{$item}}" @if(isset($attribute['value'][$item])) value="{{$formMaster-> $item}}" @endif>
+                                        <input type="text" class="form-control  form-control-sm" id="{{$item}}" name="{{$item}}" @if(isset($attribute['value'][$item])) value="{{$attribute['value'][$item]}}" @endif @if(isset($attribute['placeholder'][$item])) placeholder="{{$attribute['placeholder'][$item]}}" @endif >
                                     @elseif($attribute['type'][$item]=='textarea')
-                                        <textarea class="form-control " id="{{$item}}" name="{{$item}}" @if(isset($attribute) && array_key_exists($item, $attribute)) {{$attribute[$item]}} @endif></textarea>
+                                        <textarea class="form-control " id="{{$item}}" name="{{$item}}" @if(isset($attribute['placeholder'][$item])) placeholder="{{$attribute['placeholder'][$item]}}" @endif>
+                                          @if(isset($attribute['value'][$item])) {{$attribute['value'][$item]}} @endif
+                                        </textarea>
                                     @else
-                                        <input type="{{$attribute['type'][$item]}}" class="form-control  form-control-sm" id="{{$item}}" name="{{$item}}" @if(isset($attribute) && array_key_exists($item, $attribute)) {{$attribute[$item]}} @endif>
+                                        <input type="{{$attribute['type'][$item]}}" class="form-control  form-control-sm" id="{{$item}}" name="{{$item}}" @if(isset($attribute['value'][$item])) value="{{$attribute['value'][$item]}}" @endif @if(isset($attribute['placeholder'][$item])) placeholder="{{$attribute['placeholder'][$item]}}" @endif>
                                     @endif
                                 @endif
                             </div>
