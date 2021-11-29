6c73171b52babb812d1c56b611f143438ba40f99
RDMarwein <dapbor88@gmail.com>
2021-11-30 00:33:35 +0530

    added formajax

--- src/Http/Controllers/FormBuilderController.php
+++ src/Http/Controllers/FormBuilderController.php
@@ -43,7 +43,7 @@ class FormBuilderController extends Controller
         }
         
         $exclude=json_decode($formMaster->exclude, true);
-        if($formMaster->view=='formbuilder')
+        if($formMaster->view=='formbuilder' || $formMaster->view=='formajax')
         {
             return view('formbuilder::formbuilder.index',compact('columns','formMaster','select','exclude','model'));
         }
@@ -124,7 +124,7 @@ class FormBuilderController extends Controller
         }
         $exclude=json_decode($formMaster->exclude, true);
         $attribute=json_decode($formMaster->attribute, true);
-        if($formMaster->view=='formbuilder')
+        if($formMaster->view=='formbuilder' || $formMaster->view=='formajax')
         {
             return view('formbuilder::formbuilder.edit',compact('columns','formMaster','select','exclude','attribute','model'));
         }
@@ -191,5 +191,25 @@ class FormBuilderController extends Controller
         
         $exclude=json_decode($formMaster->exclude, true);
         return view('formbuilder::formajax.ajax',compact('columns','formMaster','select','exclude','model'));
+    }
+
+    public function finalize($id,$tid,Request $request)
+    {
+        $formPopulate=FormPopulate::select('id')
+        ->where('header',$request->field)
+        ->first();
+        $model=FormPopulate::findOrFail($formPopulate->id);
+        $values='App\\'.$model->model;
+        $data=$values::findOrFail($tid);
+        $data->finalize=true;
+        $data->save();
+        if(isset($request->redirect) && $request->redirect!='')
+        {
+            return redirect($request->redirect)->with('data',$data);
+        }
+        else
+        {
+            return redirect()->back()->with('message', 'Updated Successfully');
+        }
     }
 }
