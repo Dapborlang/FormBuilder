712e580396bd2b9812eced3116c507023bb80af9
Dapborlang <dapbor88@gmail.com>
2021-12-02 16:56:33 +0530

    change in Route

--- src/FormRole.php
+++ src/FormRole.php
@@ -15,10 +15,9 @@ class FormRole extends Model
     {
        return $this->belongsTo('App\User');
     }
-    
-    
-}
-public function link()
+    public function link()
     {
        return $this->hasMany('Rdmarwein\Formbuilder\FormMaster','role','role')->orderBy('id');
     }
+    
+}
